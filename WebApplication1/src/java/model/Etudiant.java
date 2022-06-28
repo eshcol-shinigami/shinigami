dd67972ea62f367fde73dc24d131fe6c09d2634f
eshcol-shinigami <eshcolshinigami2@gmail.com>
2022-06-28 11:00:03 +0100

    modification du 28_06_2022. ici japprend a travailer sur git et github

---
+++ WebApplication1/src/java/model/Etudiant.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author SHINIGAMI_ESHCOL
+ */
+public class Etudiant {
+      
+    public String nom;
+    public int matricule;
+    
+    public Etudiant(){
+    
+       
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public int getMatricule() {
+        return matricule;
+    }
+
+    public void setMatricule(int matricule) {
+        this.matricule = matricule;
+    }
+    
+    
+    
+    
+}
